b2dc53f1c34fb2a5e6df9848685d2e2b900aa03b
jinook929 <jinook929@gmail.com>
2021-02-16 10:08:59 -0500

    commit tree b7d152134d0d676379ba48a403667206c5884d72

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,24 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list
+    self.actors.collect {|actor|
+      actor.full_name
+    }
+    # Array.new.tap {|arr|
+    #   self.actors.each {|actor|
+    #     arr.push(actor.full_name)
+    #   }
+    # }
+  end
+
+  # def build_network(option={})
+  #   self.network_id = Network.create_or_find_by(option).id
+  # end
+
+  # def network
+  #   Network.find(self.network_id)
+  # end
 end
